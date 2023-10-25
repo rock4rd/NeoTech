ffb64e3f93e341b0a78a68a419c675aa912d20bb
cbatuic <cbalaman@uic.edu.ph>
2023-10-26 07:47:32 +0800

    added display function

--- src/App.vue
+++ src/App.vue
@@ -20,41 +20,15 @@
           </tbody>
         </table>
       </div>
-      <div>
-        <div>
-          <h2>Add Category</h2>
-          <form @submit.prevent="addCategory">
-            <div class="form-group">
-              <label for="addName">Name:</label>
-              <input v-model="newCategory.name" id="addName" class="form-control" />
-            </div>
-            <div class="form-group">
-              <label for="addDescription">Description:</label>
-              <input v-model="newCategory.description" id="addDescription" class="form-control" />
-            </div>
-            <button type="submit" class="btn btn-primary">Add</button>
-          </form>
-        </div>
-      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
 
-const categories = ref([
+const categories = [
   { id: 1, name: "Allowance", description: "From Parents" },
   { id: 2, name: "Donation", description: "From Friends" },
-]);
+];
 
-const newCategory = ref({ name: "", description: "" });
-
-const addCategory = () => {
-  if (newCategory.value.name && newCategory.value.description) {
-    const newId = categories.value.length > 0 ? Math.max(...categories.value.map((c) => c.id)) + 1 : 1;
-    categories.value.push({ id: newId, ...newCategory.value });
-    newCategory.value = { name: "", description: "" };
-  }
-}
 </script>
