eacf20eb879e397872d0f391c3f0448f5c253b8d
cbatuic <cbalaman@uic.edu.ph>
2023-11-15 07:35:46 +0800

    updated navbar

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,9 @@
 import NavigationBar from './NavigationBar.vue'
 </script>
 <template>
+    <div>
     <NavigationBar />
     <h1>Home</h1>
     <a href="/login" class="btn btn-primary">Login</a>
+    </div>
 </template>
