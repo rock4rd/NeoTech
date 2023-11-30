6d4ea6e4a5ac360d64acbccf536f67bcd457e64f
rock4rd <rdagohoy_220000000565@uic.edu.ph>
2023-11-30 12:03:25 +0800

    Update Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -84,7 +84,7 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background-image: url('./public/uicbg.jpg');
+  background-image: url('./uicbg.jpg');
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
