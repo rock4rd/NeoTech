8f41636e6ee4c55d23bfc08573a2be022b03304d
rock4rd <rdagohoy_220000000565@uic.edu.ph>
2023-12-05 10:58:38 +0800

    login page update

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,13 +5,16 @@
 
 
 <div class="background-container">
-        <div class="header">
-    <img src="/logo.png" alt="logo">
-    <h1>NeoTech</h1>
-    
-        </div>
 
-   <div class="container">
+   
+  
+      <div class="container" >
+           <div class="header">
+              
+            <img src="/logo1.png" alt="logo">
+    
+            </div>
+
         <div class="user">
             <label for="userInput">USERNAME</label>
             <input type="text" class="userInput" v-model="username" @input="handleInput">
@@ -26,15 +29,29 @@
           
             <button @click="login" class="loginbtn">Login</button>
 
+            
+
         </div>
+        <div class="terms-checkbox">
+
+<input type="checkbox" class="termsCheckbox" v-model="agreedToTerms">
+<label for="termsCheckbox" class="termsLabel">I agree to the <a href="https://www.uic.edu.ph/terms-of-use/" target="_blank ">Terms of Use</a> and <a href="https://www.uic.edu.ph/privacy-policy/" target="_blank">Privacy Policy </a> of the University of Immaculate Conception</label>
+
+</div>
    </div>
 
-   <div class="terms-checkbox">
+   <div class="helloworld">
 
-      <input type="checkbox" class="termsCheckbox" v-model="agreedToTerms">
-      <label for="termsCheckbox" class="termsLabel">I agree to the <a href="https://www.uic.edu.ph/terms-of-use/" target="_blank ">Terms of Use</a> and <a href="https://www.uic.edu.ph/privacy-policy/" target="_blank">Privacy Policy </a> of the University of Immaculate Conception</label>
+    <h2 class="helloheader">Lab Control System</h2>
 
-    </div>
+        <p>Welcome to Lab Control System</p>
+        <p>Experience streamlined laboratory management with the Lab Control System, providing a user-friendly interface for efficient equipment control, experiment coordination, and data organization.
+
+        </p>
+
+   </div>
+
+   
    
    <link rel="icon" type="image/png" :href="faviconPath" />
 
@@ -78,13 +95,16 @@ export default {
 
 
 <style>
+
+@import url('https://fonts.googleapis.com/css2?family=Suez+One&display=swap');
+
 .background-container {
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background-image: url('./uicbg.jpg');
+  background-image: url('./uicbg.jpg') ;
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
@@ -92,11 +112,15 @@ export default {
 .background-container .container {
   position: relative;
   top: 30px;
-  right: 10px;
-  height: 500px;
-  width: 850px;
-  border: 1px solid black; /* corrected border property */
-  background-color: rgba(217,217,217, 0.7);
+  right: 18%;
+  height: 90%;
+  width: 50%;
+  box-shadow: 1px solid black;
+  background-color:lightgray 50% / cover no-repeat;
+  
+  box-shadow: -1.44px 0px 5.76px 0px rgba(255, 255, 255, 0.15) inset, 0px 11.52px 8.64px 0px rgba(0, 0, 0, 0.05);
+  border-radius: 34px;
+  backdrop-filter: blur(71.99995422363281px);
   
 }
   .user, .pass{
@@ -112,6 +136,7 @@ export default {
     display: inline;
     margin-bottom: 5px;
     margin-right: 20px;
+    color: black;
    
 
   }
@@ -133,7 +158,11 @@ img{
     left: 50px;
     top: 50px;
 }
-.header{
+.container .header img{
+  position: relative;
+  left: 30%;
+height: 80%;
+width: 40%;
 
 }
 .header h1{
@@ -163,13 +192,13 @@ left: 350px;
   display: flex;
   align-items: center;
   position: relative;
-  background-color: #f3efef;
+  background-color: none;
   width: 850px;
-  left: 26.7%;
-  top: 75px;
+  
+ top: 30%;
   
  
-  border: solid black 1px;
+  border: none;
   padding: 10px; /* Adjust padding as needed */
 }
 
@@ -186,5 +215,36 @@ left: 350px;
   margin: 0; /* Remove default margin */
   right: 80px;
   white-space: nowrap;
+}
+.helloworld{
+  position: relative;
+  left: 65%;
+  bottom: 60%;
+  height: 30%;
+  width: 30%;
+  
+}
+.helloworld h2{
+  position: relative;
+  left: 5.5%;
+  bottom: 15%;
+color: #DD2A70;
+font-family: Suez One;
+font-size: 55px;
+font-style: normal;
+font-weight: bold;
+line-height: normal;
+
+}
+.helloworld p{
+  color: black;
+margin-bottom: 6%;
+text-align: center;
+font-family: Suez One;
+font-size: 30px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+font-variant: all-small-caps;
 }
 </style>
