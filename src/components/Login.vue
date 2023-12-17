8046d871aa4027082e6620671d196cb6c256c338
rock4rd <142190641+rock4rd@users.noreply.github.com>
2023-12-18 06:57:28 +0800

    Update Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -104,7 +104,7 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background-image: url('./uicbg.jpg') ;
+  background-image: url('./public/uicbg.jpg') ;
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
