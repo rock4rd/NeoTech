fe7fa2e0788bca7c84dcbcb990dbae4e642c2075
rock4rd <142190641+rock4rd@users.noreply.github.com>
2024-05-15 00:41:21 +0800

    asd

--- src/router.js
+++ src/router.js
@@ -4,7 +4,6 @@ import History from './components/History.vue';
 import Profile from './components/Profile.vue';
 import Login from './components/Login.vue';
 import Booking from './components/Booking.vue';
-import Bookreq from './components/Bookreq.vue';
 import UserView from './components/UserView.vue';
 
 
@@ -14,7 +13,6 @@ const routes = [
     { path: '/history', component: History },
     { path: '/profile', component: Profile },
     { path: '/booking', component: Booking },
-    { path: '/bookreq', component: Bookreq },
     { path: '/userview', component: UserView },
     { path: '/home', component: Home, name: "home" },
     {
